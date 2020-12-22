5357ce2fc81163e96bee8a5faa105c595eb7119f
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-22 11:54:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [birthday, setBirthday] = useState();
+  const [luckyNumber, setLuckNumber] = useState(0);
+
+  // function onChangeLuckyNumber(event) {
+  //   const luckyNumber = event.target.value;
+  //   setLuckNumber(luckyNumber);
+  // }
+
+  // function onChangeBirthday(event) {
+  //   const birthday = event.target.value;
+  //   setBirthday(birthday);
+  }
+  function isLucky() {
+    console.log(birthday);
+    console.log(luckyNumber);
+  }
+  return (
+    <div className="App">
+      <input
+        id="lukyNumber"
+        type="Date"
+        onChange={(event) => setLuckNumber(event.target.value)}
+      ></input>
+      <input id="birthday" type="number" onChange={(event) => setBirthday(event.target.value))}></input>
+      <button onClick={isLucky}>is your birthday lucky?</button>
+      <div id="output"></div>
+    </div>
+  );
+}
